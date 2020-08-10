052c602b90c93a5c80741743b471c9fd743b3993
messi <52486846@qq.com>
2020-08-10 11:42:31 +0800

    update

--- retailer-web/src/main.js
+++ retailer-web/src/main.js
@@ -27,7 +27,8 @@ import {
   Col,
   Table,
   TableColumn,
-  Dialog
+  Dialog,
+  Autocomplete
 } from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 
@@ -60,6 +61,7 @@ Vue.use(Col);
 Vue.use(Table);
 Vue.use(TableColumn);
 Vue.use(Dialog);
+Vue.use(Autocomplete);
 
 new Vue({
   router,
